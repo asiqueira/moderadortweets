99ad1a68e742772d2b924f377692841692ae0472
Alexandre Siqueira <asiqueira@globosat.com.br>
2020-07-30 18:10:20 -0300

    Armazenando tweets coletados no banco de dados.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const sqlite3 = require('sqlite3');
 const Twit = require('twit');
 
 const app = express();
+
+//CONEXÃO COM O BANCO DE DADOS
+let db = new sqlite3.Database('./db/moderadortweets.db');
+console.log('Conexão com o banco de dados aberta');
 
 //HASHTAG CUJOS TWEETS SERÃO COLETADOS
 var nm_hashtag = '#love';
@@ -20,8 +25,16 @@ const T = new Twit({
 console.log('---------- INICIANDO COLETA DE TWEETS ----------');
 stream = T.stream('statuses/filter', { track: nm_hashtag });
 
+//INSERINDO OS TWEETS COLETADOS NO BANCO DE DADOS
 stream.on('tweet', function (tweet) {
-    console.log(tweet);
+    console.log(tweet.text);
+
+    //INSERINDO OS TWEETS NO BANCO DE DADOS
+    db.run(`INSERT INTO tweets(nm_screen_name, nm_url_imagem, tx_mensagem, nm_status) VALUES(?, ?, ?, ?)`, [tweet.user.screen_name, tweet.user.profile_image_url, tweet.text, 'Em moderação'], function(err) {
+        if (err) {
+            return console.log(err.message);
+        }
+    });
 });
 
 //HOME DA APLICAÇÃO
