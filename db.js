c1c262170a2afab67c1bc78faa04534369477b51
Alexandre Siqueira <asiqueira@globosat.com.br>
2020-07-30 17:56:52 -0300

    Criando banco de dados SQLite com o arquivo db.js.

---
+++ db.js
@@ -0,0 +1,14 @@
+const sqlite3 = require('sqlite3');
+
+//CONEXÃO COM O BANCO DE DADOS
+let db = new sqlite3.Database('./db/moderadortweets.db');
+console.log('Conexão com o banco de dados aberta');
+
+//TABELA QUE ARMAZENARÁ OS TWEETS
+db.run('DROP TABLE IF EXISTS tweets');
+db.run('CREATE TABLE IF NOT EXISTS tweets (id_tweet INTEGER PRIMARY KEY, nm_screen_name TEXT NULL, nm_url_imagem TEXT NULL, tx_mensagem TEXT NULL, nm_status TEXT NULL)');
+console.log('Tabela de tweets criada.');
+
+//FECHANDO CONEXÃO COM O BANCO DE DADOS
+db.close();
+console.log('Conexão com o banco de dados fechada.');
